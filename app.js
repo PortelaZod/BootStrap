0e136ea69f24184171495f12edcce1b200362acb
Jefferson Portela Pereira <jeffersonportela@live.com>
2024-08-30 05:47:09 -0300

    1.0

---
+++ app.js
@@ -0,0 +1,7 @@
+// const navBar = document.querySelector('.collapse navbar-collapse')
+// const navBtn = document.querySelector('.navbar-toggler')
+
+// navBtn.addEventListener('click',(e)=>{
+//     e.classList.remove('collapse')
+//     alert('oi')
+// })
