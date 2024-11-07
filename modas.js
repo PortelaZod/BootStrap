78b921153f39d50421c2d95299eba1d176250536
Jefferson Portela Pereira <jeffersonportela@live.com>
2024-11-07 15:56:37 -0300

    teste tamanhos

--- modas.js
+++ modas.js
@@ -18,6 +18,30 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+let item_modal = (x) => {
+    
+    document.querySelector('.item_modal').style.left = '0'
+    document.querySelector('.item_modal_img').src = x.img
+    document.querySelector('.item_modal_nome').innerHTML = x.nome
+    document.querySelector('.item_modal_preco').innerHTML = x.preco
+
+    let gtam = x.grade
+   
+    // gtam.forEach((e) =>{
+        
+    //     let clone_btn = document.querySelector('.modelo_btn').cloneNode(true)
+    //     document.querySelector('.item_modal_grade').append(clone_btn)
+    //     clone_btn.querySelector('.btn_grade').value = e[0]
+    //     console.log(e)
+    // })
+
+    for (const element in gtam) {
+        console.log(`ola ${gtam[element]}`)
+                let clone_btn = document.querySelector('.modelo_btn').cloneNode(true)
+                document.querySelector('.item_modal_grade').append(clone_btn)
+                clone_btn.querySelector('.btn_grade').value = gtam[element]
+    }
+}
 
 
 const querySnapshot = await getDocs(collection(db, "NACIONAL"));
@@ -30,7 +54,11 @@ querySnapshot.forEach((doc) => {
     itemPlaceholder.querySelector('.item-preco').innerHTML = `R$ ${item.preco}`
     // itemPlaceholder.querySelector('.cod_item').innerHTML = `#${item.cod}`
     let grade = `${item.grade}`
-    itemPlaceholder.querySelector('.tamanho_item').innerHTML = grade.replaceAll(',',' | ')
+    itemPlaceholder.querySelector('.tamanho_item').innerHTML = grade.replaceAll(',', ' | ')
+
+    itemPlaceholder.querySelector('.addBtn2').addEventListener('click',()=>{
+        item_modal(item)
+    })
 });
 
 
@@ -43,32 +71,42 @@ let toastFunction = () => {
     }, 1500);
 }
 
+// const addSacolaBtn = document.querySelectorAll('.addBtn2')
+// addSacolaBtn.forEach(e=>
+//     e.addEventListener('click',()=>{
+//         let a = e.parentElement.parentElement.parentElement
+//         document.querySelector('.item_modal').style.left='0'
+//         document.querySelector('.item_modal_img').src = a.querySelector('.item-img').src
+//         document.querySelector('.item_modal_nome').innerHTML = a.querySelector('.item-nome').innerHTML
+//         document.querySelector('.item_modal_preco').innerHTML = a.querySelector('.item-preco').innerHTML
+
+//     })  
+// )
 
 
-const addSacolaBtn = document.querySelectorAll('.addBtn2')
+// const addSacolaBtn = document.querySelectorAll('.addBtn2')
 
-for (let i = 0; i < addSacolaBtn.length; i++) {
+// for (let i = 0; i < addSacolaBtn.length; i++) {
 
-    const element = addSacolaBtn[i];
-    element.addEventListener('click', () => {
-        const itemNome = element.parentElement.parentElement.querySelector('.item-nome').innerText
-        const itemPreco = element.parentElement.parentElement.querySelector('.item-preco').innerText
-        const itemImg = element.parentElement.parentElement.parentElement.querySelector('.item-img').src
-        let qtd = 1
+//     const element = addSacolaBtn[i];
+//     element.addEventListener('click', () => {
+//         // const itemNome = element.parentElement.parentElement.querySelector('.item-nome').innerText
+//         // const itemPreco = element.parentElement.parentElement.querySelector('.item-preco').innerText
+//         // const itemImg = element.parentElement.parentElement.parentElement.querySelector('.item-img').src
+//         // let qtd = 1
 
-        let item = {
-            nome: itemNome,
-            preco: itemPreco,
-            img: itemImg,
-            qtd: qtd
-        }
+//         // let item = {
+//         //     nome: itemNome,
+//         //     preco: itemPreco,
+//         //     img: itemImg,
+//         //     qtd: qtd
+//         // }
 
-        toastFunction()
-        addSacola(item)
+//         // toastFunction()
+//         // addSacola(item)
+//     })
 
-    })
-
-};
+// };
 
 
 let sacola = []
@@ -95,16 +133,15 @@ imgs.forEach(e =>
     }))
 
 let zoom = document.querySelectorAll('.zoom')
-    zoom.forEach(e =>
-        e.addEventListener('click', () => {
-            document.querySelector('.img_modal').style.scale = '1'
-            document.querySelector('.img_zoom').src = e.parentElement.querySelector('.item-img').src
+zoom.forEach(e =>
+    e.addEventListener('click', () => {
+        document.querySelector('.img_modal').style.scale = '1'
+        document.querySelector('.img_zoom').src = e.parentElement.querySelector('.item-img').src
     }))
 
 let x = document.querySelectorAll('.x')
-    x.forEach(e =>
-        e.addEventListener('click', () => {
-            document.querySelector('.img_modal').style.scale = '0'
+x.forEach(e =>
+    e.addEventListener('click', () => {
+        document.querySelector('.img_modal').style.scale = '0'
     }))
 // Efeito Modal nas Imagens
-    
