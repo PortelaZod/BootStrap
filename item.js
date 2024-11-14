aeaaa46449c9c834a7a5396b39bb9e576ce9c2ab
Jefferson Portela Pereira <jeffersonportela@live.com>
2024-11-14 04:35:48 +0100

    muitas horas de correções

--- item.js
+++ item.js
@@ -1,76 +1,76 @@
+//distribuir as propriedades do objeto pela pagina 
+let item = JSON.parse(localStorage.item);
+document.querySelector('.item_modal_img').src = item.img;
+document.querySelector('.item_modal_nome').innerHTML = item.nome;
+document.querySelector('.item_modal_preco').innerHTML = `R$ ${item.preco}`;
+let gtam = item.grade;//distribuir as propriedades do objeto pela pagina 
+
 //obter grade do item selecionado pelo usuário
 let tamanho_item = ''
 let grade_item = (x) => {
     tamanho_item = x
-}
-//obter grade do item selecionado pelo usuário
-
-//abre modal que mostra os dados do item clicado
-let item_modal = (x) => {
-    document.querySelector('.item_modal').style.left = '0'
-    document.querySelector('.item_modal_img').src = x.img
-    document.querySelector('.item_modal_nome').innerHTML = x.nome
-    document.querySelector('.item_modal_preco').innerHTML = `R$ ${x.preco}`
-    let gtam = x.grade
-    //seleção de tamanho do item
-
-    for (const element in gtam) {
-        let clone_btn = document.querySelector('.modelo_btn').cloneNode(true)
-        document.querySelector('.item_modal_grade').append(clone_btn)
-        clone_btn.querySelector('.btn_grade').value = gtam[element]
-        clone_btn.querySelector('.btn_grade').addEventListener('click', () => {
-            grade_item(gtam[element])
-        })
-    }//seleção de tamanho do item
-
-    //fechar modal de item
-    let voltar_btn = document.querySelector('.voltar_btn').addEventListener('click', () => {
-        location.reload()
-    })//fechar modal de item
-
-    //adicionar item na sacola e fechar modal
-    let adicionar_item = document.querySelector('.adicionar_item').addEventListener('click', () => {
-        let item = {
-            nome: x.nome,
-            preco: x.preco,
-            img: x.img,
-            qtd: 1,
-        }
-
-        if (gtam.length == 1) {
-            item.grade = gtam[0]
-            addSacola(item)
-            location.reload()
-        } else if (tamanho_item == "") {
-            alert('Selecione um tamanho')
-        } else {
-            item.grade = tamanho_item;
-            addSacola(item)
-            location.reload()
-        }
-
-    })//adicionar item na sacola e fechar modal
-
-}//abre modal que mostra os dados do item clicado
-
-
-let item = JSON.parse(localStorage.item)
-document.querySelector('.item_modal_img').src = item.img
-document.querySelector('.item_modal_nome').innerHTML = item.nome
-document.querySelector('.item_modal_preco').innerHTML = `R$ ${item.preco}`
-let gtam = item.grade
+}//obter grade do item selecionado pelo usuário
 
 //seleção de tamanho do item
 for (const element in gtam) {
-    let clone_btn = document.querySelector('.modelo_btn').cloneNode(true)
-    document.querySelector('.item_modal_grade').append(clone_btn)
-    clone_btn.querySelector('.btn_grade').value = gtam[element]
-    clone_btn.querySelector('.btn_grade').addEventListener('click', () => {
+    let btn = document.createElement('button')
+    btn.innerHTML = gtam[element]
+    document.querySelector('.item_modal_grade').append(btn)
+    btn.className += 'btn_grade btn btn-dark mx-1 fw-medium'
+    
+    btn.addEventListener('click', () => {
         grade_item(gtam[element])
+        btn_padrão()
+        btn.classList.remove('btn-dark')
+        btn.classList.add('border-dark')
     })
+}
+function btn_padrão(){
+    let btns = document.querySelectorAll('.btn_grade')
+    btns.forEach(e=>{
+        e.classList.add('btn-dark')
+    } )
 }//seleção de tamanho do item
 
 //fechar modal de item
 let voltar_btn = document.querySelector('.voltar_btn').addEventListener('click', () => {
     history.back()
 })//fechar modal de item
+
+ //função que adiciona os itens no localStorage para ser resgatados na página da Sacola.
+ let sacola = []
+ const addSacola = (x) => {
+ 
+     if (localStorage.arrItens) {
+         sacola = JSON.parse(localStorage.getItem('arrItens'))
+     }
+ 
+     let novoItem = x
+     sacola.push(novoItem)
+     localStorage.arrItens = JSON.stringify(sacola)
+ } //função que adiciona os itens no localStorage para ser resgatados na página da Sacola.
+ 
+//adicionar item na sacola e fechar modal
+let adicionar_item = document.querySelector('.adicionar_item').addEventListener('click', () => {
+    let obj = {
+        nome: item.nome,
+        preco: item.preco,
+        img: item.img,
+        qtd: 1,
+    }
+
+    if (gtam.length == 1) {
+        obj.grade = gtam[0]
+        addSacola(obj)
+        history.back()
+    } else if (tamanho_item == "") {
+        alert('Selecione um tamanho')
+    } else {
+        obj.grade = tamanho_item;
+        addSacola(obj)
+        console.log(obj)
+        history.back()
+    }
+})//adicionar item na sacola e fechar modal
+
+
--- moda_importada_40.01.js
+++ moda_importada_40.01.js
@@ -18,75 +18,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-//obter grade do item selecionado pelo usuário
-let tamanho_item = ''
-let grade_item = (x)=>{
-    tamanho_item = x
-}
-//obter grade do item selecionado pelo usuário
-
-//abre modal que mostra os dados do item clicado
-let item_modal = (x) => {
-    document.querySelector('.item_modal').style.left = '0'
-    document.querySelector('.item_modal_img').src = x.img
-    document.querySelector('.item_modal_nome').innerHTML = x.nome
-    document.querySelector('.item_modal_preco').innerHTML = `R$ ${x.preco}`
-    let gtam = x.grade
-
-    //seleção de tamanho do item
-    for (const element in gtam) {
-                let clone_btn = document.querySelector('.modelo_btn').cloneNode(true)
-                document.querySelector('.item_modal_grade').append(clone_btn)
-                clone_btn.querySelector('.btn_grade').value = gtam[element]
-                clone_btn.querySelector('.btn_grade').addEventListener('click',()=>{
-                    grade_item(gtam[element])
-                })
-    }//seleção de tamanho do item
-
-    //fechar modal de item
-    let voltar_btn = document.querySelector('.voltar_btn').addEventListener('click',()=>{
-        location.reload()
-    })//fechar modal de item
-
-    //adicionar item na sacola e fechar modal
-    let adicionar_item = document.querySelector('.adicionar_item').addEventListener('click',()=>{
-                let item = {
-                    nome: x.nome,
-                    preco: x.preco,
-                    img: x.img,
-                    qtd: 1,
-                }
-
-                if(gtam.length == 1){
-                    item.grade = gtam[0]
-                    addSacola(item)
-                    location.reload()
-                }else if(tamanho_item == ""){
-                    alert('Selecione um tamanho')
-                }else{
-                    item.grade = tamanho_item;
-                    addSacola(item)
-                    location.reload()
-                }
-
-    })//adicionar item na sacola e fechar modal
-
-}//abre modal que mostra os dados do item clicado
-
-
- //função que adiciona os itens no localStorage para ser resgatados na página da Sacola.
-let sacola = []
-const addSacola = (x) => {
-
-    if (localStorage.arrItens) {
-        sacola = JSON.parse(localStorage.getItem('arrItens'))
-    }
-
-    let novoItem = x
-    sacola.push(novoItem)
-    localStorage.arrItens = JSON.stringify(sacola)
-} //função que adiciona os itens no localStorage para ser resgatados na página da Sacola.
-
 const querySnapshot = await getDocs(collection(db, "IMPORTADAS_40.01"));
 querySnapshot.forEach((doc) => {
     const item = doc.data()
@@ -100,7 +31,7 @@ querySnapshot.forEach((doc) => {
     itemPlaceholder.querySelector('.tamanho_item').innerHTML = grade.replaceAll(',', ' | ')
 
     itemPlaceholder.querySelector('.addBtn2').addEventListener('click',()=>{
-        item_modal(item)
+        
     })
 });
 
