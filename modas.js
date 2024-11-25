69ce07c131bed7b104e4224c9d4a2b2a4e92892f
Jefferson Portela Pereira <jeffersonportela@live.com>
2024-11-25 13:38:58 -0300

    gerenciamento pedidos

--- modas.js
+++ modas.js
@@ -13,8 +13,71 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const querySnapshot = await getDocs(collection(db, "NACIONAL"));
-querySnapshot.forEach((doc) => {
-    const item = doc.data()
+    
+// querySnapshot.forEach((doc) => {
+//     let item = JSON.parse(localStorage.ftc)
+//     const itemPlaceholder = document.querySelector('.itemPlaceholder').cloneNode(true)
+//     const itemArea = document.querySelector('.itens-area').append(itemPlaceholder)
+//     itemPlaceholder.querySelector('.item-img').src = item.img
+//     itemPlaceholder.querySelector('.item-nome').innerHTML = item.nome
+//     itemPlaceholder.querySelector('.item-preco').innerHTML = `R$ ${item.preco}`
+//     // itemPlaceholder.querySelector('.cod_item').innerHTML = `#${item.cod}`
+//     let grade = `${item.grade}`
+//     itemPlaceholder.querySelector('.tamanho_item').innerHTML = grade.replaceAll(',', ' | ')
+
+//     //adiciona o item no localStorage pra ser resgatado na aba sacola 
+//     itemPlaceholder.querySelector('.addBtn2').addEventListener('click',()=>{
+//         localStorage.item = JSON.stringify(item)
+//         location.href = './item.html'
+//     })//adiciona o item no localStorage pra ser resgatado na aba sacola
+
+//     let url = JSON.parse(localStorage.ftc)
+
+//     console.log(url)
+    
+//     // fetch(url)
+//     // .then((x)=>{
+//     //    return x
+//     // }).then((data)=>{
+//     //     let d = data.nome
+//     // })
+
+// });
+
+
+//----------------------------------
+
+
+let itens = [{"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},{"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"}]
+let url = './P.html'
+
+document.querySelector('.pqp_btn').addEventListener('click',()=>{
+   
+    pqp()
+    document.querySelector('#moda1').src='/P.js'
+})
+
+let scr = {
+    scr1: './P.js',
+    scr2: './modas.js'
+}
+
+localStorage.scr = JSON.stringify(scr)
+localStorage.scr
+
+function  pqp(){
+
+    fetch(url)
+    .then(res => res.text())
+    .then(data=>{
+        document.querySelector('body').innerHTML=data
+    })
+
+}
+
+
+itens.forEach( e=>{
+    let item = e 
     const itemPlaceholder = document.querySelector('.itemPlaceholder').cloneNode(true)
     const itemArea = document.querySelector('.itens-area').append(itemPlaceholder)
     itemPlaceholder.querySelector('.item-img').src = item.img
@@ -23,13 +86,19 @@ querySnapshot.forEach((doc) => {
     // itemPlaceholder.querySelector('.cod_item').innerHTML = `#${item.cod}`
     let grade = `${item.grade}`
     itemPlaceholder.querySelector('.tamanho_item').innerHTML = grade.replaceAll(',', ' | ')
-
+    
     //adiciona o item no localStorage pra ser resgatado na aba sacola 
     itemPlaceholder.querySelector('.addBtn2').addEventListener('click',()=>{
         localStorage.item = JSON.stringify(item)
         location.href = './item.html'
     })//adiciona o item no localStorage pra ser resgatado na aba sacola
-});
+    
+})
+
+
+
+
+//----------------------------------
 
 // Efeito Modal nas Imagens
 let imgs = document.querySelectorAll('.item-img')
@@ -70,3 +139,45 @@ x.forEach(e =>
         document.querySelector('.img_modal').style.scale = '0'
     }))// fechar zoom Modal nas Imagens
 // Efeito Modal nas Imagens
+
+    let xxx = [
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"G","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"G","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"G","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+ {"cod":"9519519519519","nome":"Camiseta John John","grade":"P","quantidade":"1","img":"https://firebasestorage.googleapis.com/v0/b/teste-com-firebase-c28bd.appspot.com/o/WhatsApp%20Image%202024-11-04%20at%2019.40.20.jpeg?alt=media&token=db780df8-d5af-4ec1-9ba7-bec7e5660f92","colecao":"NACIONAL","preco":"54.90"},
+]
+
+
+
+
+
